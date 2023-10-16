a6f2f157175484337094c450f4b30fca13fb6bea
Mark Saville <mark.saville@outlook.com>
2023-10-16 14:55:50 +0200

    store login data in local storage

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -31,7 +31,9 @@ ngOnInit(): void {
 // This is the function responsible for sending the form inputs to the backend
 loginUser(): void {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
-  // Logic for a successful user login goes here! (To be implemented)
+  // Logic for a successful user login goes here!
+    localStorage.setItem('user', result.user.Username);
+    localStorage.setItem('token', result.token);
      this.dialogRef.close(); // This will close the modal on success!
      this.snackBar.open(result, 'OK', {
         duration: 2000
