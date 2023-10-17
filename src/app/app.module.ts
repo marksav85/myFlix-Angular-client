519b41ceaa4b2600d2257837a81aee17cfdd6612
Mark Saville <mark.saville@outlook.com>
2023-10-17 19:27:02 +0200

    add profile to navigation path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
