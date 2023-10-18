28d4e41dcd96cebfd0848b983eeeb5a9cdc05655
Mark Saville <mark.saville@outlook.com>
2023-10-18 12:17:10 +0200

    update navigation routes

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -4,25 +4,15 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.scss']
+  styleUrls: ['./navigation-bar.component.scss'],
 })
 export class NavigationBarComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void { }
-
-  toMovies(): void {
-    this.router.navigate(['movies']);
-  }
-
-  toProfile(): void {
-    this.router.navigate(['profile']);
-  }
+  ngOnInit(): void {}
 
   toLogout(): void {
     this.router.navigate(['welcome']);
     localStorage.clear();
   }
-
-
 }
