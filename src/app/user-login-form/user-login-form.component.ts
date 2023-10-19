82b4d316085e7233a71274ecf4ecd4aabb96d1ac
Mark Saville <mark.saville@outlook.com>
2023-10-19 10:26:25 +0200

    refactor login code

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -13,48 +13,35 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 // routing
 import { Router } from '@angular/router';
 
-
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrls: ['./user-login-form.component.scss'],
 })
-
 export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
 
-  @Input() userData = { Username: '', Password: ''};
-
-constructor(
+  constructor(
     public fetchApiData: UserRegistrationService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar,
     private router: Router
-    ) { }
+  ) {}
 
-ngOnInit(): void {
-}
+  ngOnInit(): void {}
 
-// This is the function responsible for sending the form inputs to the backend
-loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe((data) => {
-  // Logic for a successful user login goes here!
-      localStorage.setItem("user", JSON.stringify(data.user))
-      localStorage.setItem("token", data.token);
-      localStorage.setItem('Username', data.user.Username)
-    /* localStorage.setItem('user', result.user.Username);
-    localStorage.setItem('token', result.token); */
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(data, 'OK', {
-        duration: 2000
-     });
-     this.router.navigate(['movies']);
-    }, (result) => {
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
+  // This is the function responsible for sending the form inputs to the backend
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((result) => {
+      // Logic for a successful user login goes here!
+      localStorage.setItem('user', JSON.stringify(result.user));
+      localStorage.setItem('token', result.token);
+      localStorage.setItem('Username', result.user.Username);
+      this.dialogRef.close(); // This will close the modal on success!
+      this.snackBar.open('Login successful', 'OK', {
+        duration: 2000,
       });
+      this.router.navigate(['movies']);
     });
-    
   }
-
-
-  }
+}
